8189d9d2edd76753ea12695d3bacde3545fd8275
Ganesh R <108968997+GaneshRaut12@users.noreply.github.com>
2023-12-14 08:57:00 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import Root from './App';
+import App from './App';
 import reportWebVitals from "./reportWebVitals";
-import Root from "./Root";
-import TopStocks from "./TopStocks/TopStocks";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Root />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
