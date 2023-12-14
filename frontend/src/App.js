bd1daa64ce2a3efc7526c5d57d7dc8d0c22babe8
Ganesh Raut <ganesh.raut@softobiz.com>
2023-12-14 08:50:14 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import TopStocks from "./TopStocks/TopStocks";
+import Main from "./components/Main";
 
-const App = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:4000/api/top-20');
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error('Error fetching data:', error.message);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const calculateGainers = () => {
-    if (!data) return [];
-    
-    data.results.forEach(stock => {
-      stock.gain = stock.c - stock.o;
-    });
-
-    return data.results
-      .slice()
-      .sort((a, b) => b.gain - a.gain)
-      .slice(0, 20);
-  };
-
-  const topGainers = calculateGainers();
-
+function Root() {
   return (
-    <div>
-      <h1>Top 20 Gainers Today</h1>
-      {topGainers.length > 0 ? (
-        <ul>
-          {topGainers.map((stock) => (
-            <li key={stock.T}>
-              {stock.T} - Gain: {stock.gain}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/gainers" element={<TopStocks />} />
+      </Routes>
+    </>
   );
-};
+}
 
-export default App;
+export default Root;
